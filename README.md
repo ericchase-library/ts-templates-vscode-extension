84ed0403aed04365108eea29abd888b882a4b6f7
ericchase <github@eric.addymail.com>
2025-08-15 13:25:42 -0500

    up
    Signed-off-by: ericchase <github@eric.addymail.com>

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 ## About
 
-https://github.com/ericchase-library/ts-templates-vscode-extension
-
 This project houses the base Visual Studio Code extension template project that I use to create new Visual Studio Code extension projects.
 
 ## Build Tools V4
@@ -9,6 +7,7 @@ This project houses the base Visual Studio Code extension template project that 
 For information about my TypeScript library and Build Tools, please visit:
 
 - https://github.com/ericchase-library/ts-library
+- https://github.com/ericchase-library/ts-templates-vscode-extension
 
 ## Project Disclaimer
 
--- tools/pull-lib.ts
+++ tools/pull-lib.ts
@@ -1,13 +1,25 @@
 import { NODE_PATH } from '../src/lib/ericchase/NodePlatform.js';
 import { Builder } from './core/Builder.js';
+import { Step_Async } from './core/step/Step_Async.js';
+import { Step_Bun_Run } from './core/step/Step_Bun_Run.js';
 import { Step_FS_Mirror_Directory } from './core/step/Step_FS_Mirror_Directory.js';
 
+// This script pulls template lib files from template project.
+
+const template_path = 'C:/Code/Base/JavaScript-TypeScript/Templates/VSCode-Extension';
+const lib_folders: string[] = ['lib-vscode-extension'];
+
 Builder.SetStartUpSteps(
-  Step_FS_Mirror_Directory({
-    from_path: NODE_PATH.join('C:/Code/Base/JavaScript-TypeScript/Templates/VSCode-Extension', 'tools/lib-vscode-extension'),
-    to_path: NODE_PATH.join(Builder.Dir.Tools, 'lib-vscode-extension'),
-    include_patterns: ['**/*'],
-  }),
+  Step_Bun_Run({ cmd: ['bun', 'install'], showlogs: false }),
+  Step_Async(
+    lib_folders.map((dir: string) =>
+      Step_FS_Mirror_Directory({
+        from_path: NODE_PATH.join(template_path, 'tools/' + dir),
+        to_path: NODE_PATH.join(Builder.Dir.Tools, dir),
+        include_patterns: ['**/*'],
+      }),
+    ),
+  ),
   //
 );
 
