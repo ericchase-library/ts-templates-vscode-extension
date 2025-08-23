b14f33dcfabaafeb87c9c51ce110449a3a8cb109
ericchase <github@eric.addymail.com>
2025-08-22 23:21:24 -0500

    up
    Signed-off-by: ericchase <github@eric.addymail.com>

--- tools/build.ts
+++ tools/build.ts
@@ -1,4 +1,5 @@
 import { BunPlatform_Argv_Includes } from '../src/lib/ericchase/BunPlatform_Argv_Includes.js';
+import { NodePlatform_PathObject_Relative_Class } from '../src/lib/ericchase/NodePlatform_PathObject_Relative_Class.js';
 import { Step_Dev_Format } from './core-dev/step/Step_Dev_Format.js';
 import { Step_Dev_Project_Update_Config } from './core-dev/step/Step_Dev_Project_Update_Config.js';
 import { Builder } from './core/Builder.js';
@@ -6,6 +7,7 @@ import { Processor_Set_Writable } from './core/processor/Processor_Set_Writable.
 import { Processor_TypeScript_Generic_Bundler } from './core/processor/Processor_TypeScript_Generic_Bundler.js';
 import { Step_Bun_Run } from './core/step/Step_Bun_Run.js';
 import { Step_FS_Clean_Directory } from './core/step/Step_FS_Clean_Directory.js';
+import { Step_Output_Merge_Files } from './core/step/Step_Output_Merge_Files.js';
 import { Processor_JavaScript_Rollup } from './lib-vscode-extension/processors/Processor_JavaScript_Rollup.js';
 import { Step_VSCE_Package } from './lib-vscode-extension/steps/Step_VSCE_Package.js';
 
@@ -64,12 +66,62 @@ Builder.SetAfterProcessingSteps();
 
 // These steps are run during the cleanup phase only.
 Builder.SetCleanUpSteps(
-  /** Flatten the out folder for original repo. Useful for patching existing
-   * extensions. */
-  // Step_Flatten_OriginalRepo_OutDir({ original_subdir: 'original-repo', merge_list: ['package.json', 'CHANGELOG.md', 'README.md'] }),
+  /**
+   * When continuously patching an existing extension, store its repository
+   * files under `src/original-repo`. This let's you merge specific JSON and
+   * text files between the original code base and yours using the
+   * `Step_Output_Merge_Files` step. Then make sure to relocate the rest of the
+   * original repo output folder onto the top-level output folder using the
+   * `Step_FS_Move_Files` step.
+   */
+  // Step_Output_Merge_Files(
+  //   {
+  //     type: 'json',
+  //     merge_files: ['original-repo/package.json', 'package.json'],
+  //     out_file: 'package.json',
+  //     modify: (data: any) => {
+  //       /**
+  //        * Note: This is effectively the only way to delete properties.
+  //        */
+  //       // remove the spyware
+  //       delete data.contributes.configuration.properties['code-runner.enableAppInsights'];
+  //       delete data.dependencies.applicationinsights;
+  //       // these are for dev only
+  //       delete data.devDependencies;
+  //       delete data.scripts;
+  //       /**
+  //        * Note: You can add and modify values directly in `src/package.json`.
+  //        */
+  //       // make sure the main script is a relative `.js` file in posix form
+  //       data.main = NodePlatform_PathObject_Relative_Class(data.main).replaceExt('.js').toPosix().join({ dot: true });
+  //       // increment the original version
+  //       data.version = SEMVER_UTIL.increment(data.version, 'major');
+  //     },
+  //   },
+  //   {
+  //     type: 'text',
+  //     merge_files: ['CHANGELOG.md', 'original-repo/CHANGELOG.md'],
+  //     out_file: 'CHANGELOG.md',
+  //   },
+  // ),
+  // Step_FS_Move_Files({
+  //   include_patterns: ['**'],
+  //   from_dir: `${Builder.Dir.Out}/original-repo`,
+  //   into_dir: Builder.Dir.Out,
+  //   overwrite: true,
+  // }),
 
+  Step_Output_Merge_Files({
+    type: 'json',
+    merge_files: ['package.json'],
+    out_file: 'package.json',
+    modify: (data: any) => {
+      // make sure the main script is a relative `.js` file in posix form
+      data.main = NodePlatform_PathObject_Relative_Class(data.main).replaceExt('.js').toPosix().join({ dot: true });
+    },
+  }),
   Step_Dev_Format({ showlogs: false }),
-  Step_VSCE_Package({ main: 'extension.module.ts', release_dir: 'release' }),
+  Step_VSCE_Package({ release_dir: 'release' }),
   //
 );
 
--- tools/lib-vscode-extension/steps/Step_VSCE_Package.ts
+++ tools/lib-vscode-extension/steps/Step_VSCE_Package.ts
@@ -1,11 +1,8 @@
 import { Async_BunPlatform_File_Move } from '../../../src/lib/ericchase/BunPlatform_File_Move.js';
-import { Async_BunPlatform_File_Read_Text } from '../../../src/lib/ericchase/BunPlatform_File_Read_Text.js';
 import { Async_BunPlatform_File_Write_Text } from '../../../src/lib/ericchase/BunPlatform_File_Write_Text.js';
 import { Async_BunPlatform_Glob_Scan_Generator } from '../../../src/lib/ericchase/BunPlatform_Glob_Scan_Generator.js';
 import { NODE_PATH } from '../../../src/lib/ericchase/NodePlatform.js';
-import { NodePlatform_PathObject_Relative_Class } from '../../../src/lib/ericchase/NodePlatform_PathObject_Relative_Class.js';
 import { Builder } from '../../core/Builder.js';
-import { SEMVER_UTIL } from '../../core/bundle/semver-util/semver-util.js';
 import { Logger } from '../../core/Logger.js';
 import { Step_Bun_Run } from '../../core/step/Step_Bun_Run.js';
 
@@ -18,33 +15,8 @@ class Class implements Builder.Step {
 
   constructor(readonly config: Config) {}
   async onRun(): Promise<void> {
-    // write empty .vscodeignore file, as we don't need it
+    // write empty `.vscodeignore` file, as we don't need it
     await Async_BunPlatform_File_Write_Text(NODE_PATH.join(Builder.Dir.Out, '.vscodeignore'), '');
-    // modify package.json
-    const { error, value: text } = await Async_BunPlatform_File_Read_Text(NODE_PATH.join(Builder.Dir.Out, 'package.json'));
-    if (text !== undefined) {
-      const package_json = JSON.parse(text);
-      // remove scripts and devDependencies
-      delete package_json.scripts;
-      delete package_json.devDependencies;
-      // set main
-      const main = this.config.main ?? package_json.main ?? undefined;
-      if (main !== undefined) {
-        package_json.main = NodePlatform_PathObject_Relative_Class(main).replaceExt('.js').toPosix().join({ dot: true });
-      }
-      // increment version
-      if (this.config.increment_version !== undefined) {
-        const new_version = SEMVER_UTIL.increment(package_json.version, this.config.increment_version);
-        if (new_version !== undefined) {
-          package_json.version = new_version;
-        } else {
-          this.channel.error(new Error('The package.json "version" property is not a valid semantic version.'));
-        }
-      }
-      await Async_BunPlatform_File_Write_Text(NODE_PATH.join(Builder.Dir.Out, 'package.json'), JSON.stringify(package_json, null, 2));
-    } else {
-      throw error;
-    }
     await Builder.ExecuteStep(Step_Bun_Run({ cmd: ['npm', 'install', '--omit=dev'], cwd: Builder.Dir.Out }));
     await Builder.ExecuteStep(Step_Bun_Run({ cmd: ['bun', 'run', 'vsce', 'package'], cwd: Builder.Dir.Out }));
     for await (const path of Async_BunPlatform_Glob_Scan_Generator(Builder.Dir.Out, '*.vsix')) {
@@ -54,19 +26,4 @@ class Class implements Builder.Step {
 }
 interface Config {
   release_dir: string;
-  /**
-   * Corresponds to `package.json` `main` property.
-   *
-   * Note: Not all extensions need a main script.
-   * @default undefined
-   */
-  main?: string;
-  /**
-   * Use this to increment the package.json "version" property once for this
-   * build. Useful when continuously patching an existing extension. For your
-   * own extension, update the actual "version" property of the actual
-   * package.json, instead.
-   * @default undefined
-   */
-  increment_version?: Parameters<typeof SEMVER_UTIL.increment>[1];
 }
